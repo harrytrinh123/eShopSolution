0ab2bce228e595c768233563fc9e746f979a35db
Jack Trinh <40319325+jacktrinh123@users.noreply.github.com>
2021-07-06 00:12:27 +0700

    add class entities and enum

---
+++ eShopSolution.Data/EF/EShopDbContext.cs
@@ -0,0 +1,18 @@
+﻿using eShopSolution.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Data.EF
+{
+    public class EShopDbContext : DbContext
+    {
+        public EShopDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
