59b933bcd9519761f3f72187f4d56ce71b3f30ef
Jack Trinh <40319325+jacktrinh123@users.noreply.github.com>
2021-08-04 22:03:44 +0700

    [write] add seed database

--- eShopSolution.Data/EF/EShopDbContext.cs
+++ eShopSolution.Data/EF/EShopDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using eShopSolution.Data.Configurations;
 using eShopSolution.Data.Entities;
+using eShopSolution.Data.Extensions;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -16,6 +17,7 @@ namespace eShopSolution.Data.EF
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            // Configure using Fluent API
             modelBuilder.ApplyConfiguration(new AppConfigConfiguration());
             modelBuilder.ApplyConfiguration(new ProductConfiguration());
             modelBuilder.ApplyConfiguration(new CategoryConfiguration());
@@ -29,6 +31,9 @@ namespace eShopSolution.Data.EF
             modelBuilder.ApplyConfiguration(new ProductTranslationConfiguration());
             modelBuilder.ApplyConfiguration(new PromotionConfiguration());
             modelBuilder.ApplyConfiguration(new TransactionConfiguration());
+
+            // Data seeding
+            modelBuilder.Seed();
         }
 
         public DbSet<Product> Products { get; set; }
